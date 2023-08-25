e78d2653c911555d7a030af1ab1616384ee124f0
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-25 15:34:02 +0530

    upd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Solar from "./components/Solatsystem/Solar";
 
 function App() {
   return (
     <div className="bg-black h-screen w-full flex justify-center items-center">
       <Navbar/>
+      <Solar/>
     </div>
   );
 }
