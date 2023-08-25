1ba30519a4d155d487b7bebae7d2b85a8982742f
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-25 13:08:20 +0530

    Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+// import { useThemeContext } from "../hooks/useThemeContext";
+// import Light from "./icons/Light";
+// import Logo from "./icons/Logo";
+import Bars from "./icons/Bars";
+import Close from "./icons/Close";
+// import Dark from "./icons/Dark";
+let Links = [
+  { name: "Home", link: "/#home" },
+  { name: "About", link: "#about" },
+  { name: "Skills", link: "#skills" },
+  { name: "Project", link: "#project" },
+  { name: "Blogs", link: "#blog" },
+];
+const Navbar = () => {
+  const [open, setopen] = useState(false);
+  const [theme ,changeTheme] =useState('dark');
+  return (
+    <section className={"w-full h-20 p-2 flex  justify-between items-center md:justify-around  fixed top-0 z-999"+`${theme==='dark' ? 'bg-black':'bg-background'}`}>
+      <div className="w-10 m-3">
+          Logo
+          {/* logo */}
+      </div> 
+      <div className={"absolute w-full h-auto p-5 top-20 md:static text-center "+`${theme==='dark' ? 'bg-darkmodebackground':'bg-background'}`}>
+        <ul className={"w-full flex flex-col items-center md:flex-row md:justify-center md:items-center"+`${open ? '':' hidden md:flex'}`}>
+      {
+        Links.map((link,index)=> (
+          <li className="text-2xl font-medium m-5 text-white hover:text-secondary focus:text-secondary" key={index} onClick={()=>{setopen(!open)}}>
+            <a href={link.link}>{link.name}</a>
+          </li>
+        ))
+      }
+        </ul>
+      </div>
+     <div className="flex items-center mr-2">
+      <div onClick={()=> setopen(!open)} className="md:hidden pl-4">
+        <div>
+         {open ? <Close/>: <Bars />}
+        </div>
+      </div>
+     </div>
+    </section>
+  );
+};
+export default Navbar;
---
+++ frontend/src/components/icons/Close.tsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faXmark } from "@fortawesome/free-solid-svg-icons"
+const Close = () => {
+  return (
+     <FontAwesomeIcon icon={faXmark} style={{color: "#f2f4f7",}}  size="xl"/>
+  )
+}
+
+export default Close
